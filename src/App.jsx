79fd0407dd031e7ba2b7d87b78e8b0b43a4f6e9a
Ayaanleahmed <boqolhaji5@gmail.com>
2024-05-27 20:23:34 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+
+     <h1 className="text-3xl font-bold bg-blue-500 pl-10 pt-4 text-left text-white" >Somaliya Hanolato</h1>
+      <h1 className="text-2xl font-bold underline bg-cyan-400 text-black-500">
+        Mogadisho Capital City Of Spmalia
+      </h1>
+     
+    </>
+  )
+}
+
+export default App
